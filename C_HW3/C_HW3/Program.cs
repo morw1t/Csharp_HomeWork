f650ad67a76b41492be26b8566c9321a37cb4eef
vito <vitalik.potashev@yandex.ru>
2022-03-09 23:40:09 +0300

    практическая 3

---
+++ C_HW3/C_HW3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_HW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //сдам дз чуть позже
+        }
+    }
+}
